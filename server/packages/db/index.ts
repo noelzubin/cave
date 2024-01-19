5993f14187f04826587a0154e75e63b834f3312f
noel.victor2 <noel.victor@tech.jago.com>
2024-01-20 03:43:42 +0530

    add lerna and build caching

--- server/packages/db/index.ts
+++ server/packages/db/index.ts
@@ -11,8 +11,7 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log:
-      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+    log: ["query", "error", "warn"] 
   });
 
-if (env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+globalForPrisma.prisma = prisma;
